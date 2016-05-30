c0c316b732b1c02ec9842fd14d458054a47dd232
Herman Chan <hermchan89@gmail.com>
2016-05-30 15:02:54 -0700

    edited landing page, connected three way binding between firebase and client side when creating a room name

--- public/scripts/controllers/RoomsIndexController.js
+++ public/scripts/controllers/RoomsIndexController.js
@@ -1,25 +1,30 @@
-var app = angular.module("wewatch", ["firebase"]);
-var ref = new Firebase("https://burning-inferno-6004.firebaseio.com/room");
+var app = angular.module("wewatch");
 
-app.controller("roomsIndexCtrl", function($scope, $firebaseObject) {
+app.controller("RoomsIndexController", function($scope, $firebaseObject, $document, $location) {
+  var ref = new Firebase("https://burning-inferno-6004.firebaseio.com/room");
 
+  $document.ready(function() {
+    console.log('room index document is running');
+    // ref.child("roomName").on("value", function(snapshot) {
+    //   room = snapshot.val();
+    // });
+  });
   var syncObject = $firebaseObject(ref);
   // synchronize the object with a three-way data binding
   // click on `index.html` above to see it used in the DOM!
   // It's actually saved back in the firebase database in real-time.
   syncObject.$bindTo($scope, "data");
-  console.log('getting room', "data");
+
+  ref.child("roomName").on("value", function(snapshot) {
+    room = snapshot.val();
+    console.log('trying to room', room);
+  });
+
+  // will set roomName(child) in firebase to the val of input
+  $scope.enterRoom = function (){
+    var roomId = $("#room-name").val();
+    console.log('created or joining room: ', roomId);
+    ref.set({roomName: roomId});
+
+  };
 });
-
-
-ref.child("roomName").on("value", function(snapshot) {
-  room = snapshot.val();
-  console.log('tryint o gt room', room);
-});
-
-// will set roomName(child) in firebase to the val of input
-function enterRoom(){
-  var roomId = $("#room-name").val();
-  console.log(roomId);
-  ref.set({roomName: roomId});
-}
